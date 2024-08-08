52313cf347bec7685468c60c908dcc95517d1f95
Moxin1044 <1044631097@qq.com>
2024-08-08 14:35:52 +0800

    添加项目文件。

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace XCTF赛事列表
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.GameName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.releaseSponsor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.competitionStartTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.competitionEndTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.GameName,
+            this.releaseSponsor,
+            this.competitionStartTime,
+            this.competitionEndTime});
+            this.dataGridView1.Location = new System.Drawing.Point(1, 1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(728, 338);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // GameName
+            // 
+            this.GameName.HeaderText = "赛事名称";
+            this.GameName.Name = "GameName";
+            this.GameName.ReadOnly = true;
+            this.GameName.Width = 280;
+            // 
+            // releaseSponsor
+            // 
+            this.releaseSponsor.HeaderText = "主办方";
+            this.releaseSponsor.Name = "releaseSponsor";
+            this.releaseSponsor.ReadOnly = true;
+            // 
+            // competitionStartTime
+            // 
+            this.competitionStartTime.HeaderText = "比赛开始时间";
+            this.competitionStartTime.Name = "competitionStartTime";
+            this.competitionStartTime.ReadOnly = true;
+            this.competitionStartTime.Width = 150;
+            // 
+            // competitionEndTime
+            // 
+            this.competitionEndTime.HeaderText = "比赛结束时间";
+            this.competitionEndTime.Name = "competitionEndTime";
+            this.competitionEndTime.ReadOnly = true;
+            this.competitionEndTime.Width = 150;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(730, 340);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "XCTF赛事列表";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn GameName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn releaseSponsor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn competitionStartTime;
+        private System.Windows.Forms.DataGridViewTextBoxColumn competitionEndTime;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json.Linq;
+using System.IO.Compression;
+using System.IO;
+
+namespace XCTF赛事列表
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            // 目标URL
+            string url = "https://adworld.xctf.org.cn/api/event/release_event/list/?page=1&page_size=1000&search=&isSearch=false&event_status=&event_type=";
+
+            // 请求头
+            var headers = new HttpClient();
+            headers.DefaultRequestHeaders.Add("sec-ch-ua", "\"Not)A;Brand\";v=\"99\", \"Google Chrome\";v=\"127\", \"Chromium\";v=\"127\"");
+            headers.DefaultRequestHeaders.Add("Accept", "application/json, text/plain, */*");
+            headers.DefaultRequestHeaders.Add("Accept-Language", "zh-CN");
+            headers.DefaultRequestHeaders.Add("sec-ch-ua-mobile", "?0");
+            headers.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36");
+            headers.DefaultRequestHeaders.Add("sec-ch-ua-platform", "\"macOS\"");
+            headers.DefaultRequestHeaders.Add("Sec-Fetch-Site", "same-origin");
+            headers.DefaultRequestHeaders.Add("Sec-Fetch-Mode", "cors");
+            headers.DefaultRequestHeaders.Add("Sec-Fetch-Dest", "empty");
+            headers.DefaultRequestHeaders.Add("Accept-Encoding", "gzip, deflate, br, zstd");
+            headers.DefaultRequestHeaders.Add("Connection", "keep-alive");
+            headers.DefaultRequestHeaders.Add("Host", "adworld.xctf.org.cn");
+
+            // 发送GET请求
+            HttpResponseMessage response = await headers.GetAsync(url);
+
+            // 检查响应状态码
+            if (response.IsSuccessStatusCode)
+            {
+                // 解压缩响应内容
+                Stream responseStream = await response.Content.ReadAsStreamAsync();
+                using (var decompressedStream = new GZipStream(responseStream, CompressionMode.Decompress))
+                using (var reader = new StreamReader(decompressedStream))
+                {
+                    string responseBody = await reader.ReadToEndAsync();
+                    JObject data = JObject.Parse(responseBody);
+                    JArray games = (JArray)data["data"]["rows"];
+                    foreach (var item in games)
+                    {
+                        string releaseName = item["release_name"].ToString();
+                        string releaseSponsor = item["release_sponsor"].ToString();
+                        string competitionStartTime = item["competition_start_time"].ToString();
+                        string competitionEndTime = item["competition_end_time"].ToString();
+                        dataGridView1.Rows.Add(releaseName, releaseSponsor, competitionStartTime, competitionEndTime);
+                        // MessageBox.Show($"竞赛名称：{releaseName} \n主办方：{releaseSponsor} \n比赛开始时间：{competitionStartTime} \n比赛结束时间：{competitionEndTime}");
+                    }
+                }
+            }
+            else
+            {
+                // 打印错误信息
+                string errorMessage = await response.Content.ReadAsStringAsync();
+                MessageBox.Show($"请求失败，状态码为: {response.StatusCode}\n{errorMessage}");
+            }
+        }
+    }
+}
